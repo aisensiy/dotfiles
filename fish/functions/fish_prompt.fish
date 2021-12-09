5961d0f9fbd214ad3949261dfc52ae63d0af8c8e
aisensiy <aisensiy@163.com>
2021-12-09 17:34:49 +0800

    feat(fish): show kubernetes context in right prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -29,32 +29,32 @@ function fish_prompt
     string unescape $$_tide_prompt_var[1][2..]
 end
 
-# function kubectl_status
-#   [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
-#   [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
-#   set -l config $KUBECONFIG
-#   [ -z "$config" ]; and set -l config "$HOME/.kube/config"
-#   if [ ! -f $config ]
-#     echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
-#     return
-#   end
-# 
-#   set -l ctx (kubectl config current-context 2>/dev/null)
-#   if [ $status -ne 0 ]
-#     echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
-#     return
-#   end
-# 
-#   set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
-#   [ -z $ns ]; and set -l ns 'default'
-# 
-#   echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
-# end
-# 
-# function fish_right_prompt
-#     string unescape $$_tide_prompt_var[1][1]
-#     echo (kubectl_status)
-# end
+function kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+end
+
+function fish_right_prompt
+    string unescape $$_tide_prompt_var[1][1]
+    echo (kubectl_status)
+end
 
 function _tide_on_fish_exit --on-event fish_exit
     set -e $_tide_prompt_var
